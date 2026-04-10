b098ec33ce1eec6b6b85e9d48d2d86a80b9289a0
AlteOs-Official <alteos7official@outlook.com>
2026-04-09 21:30:31 -0400

    Create CommunityHighlights.jsx

---
+++ CommunityHighlights.jsx
@@ -0,0 +1,8 @@
+export default function CommunityHighlights() {
+  return (
+    <section>
+      <h1>Community Highlights</h1>
+      <p>Contenus mis en avant par la communauté.</p>
+    </section>
+  );
+}
